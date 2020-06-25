923a72c7e5dec836be7fc4dc8acc0ef897a629f8
mikeyokeeffe <mikey.okeeffe@unipart.io>
2020-06-25 19:20:08 +0100

    Updates

---
+++ precache-manifest.78836f02fb19c53c81d90383c1468f1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4c0a468386de5b09cd8c00d4ed11e9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ae70f892148e0f733af8",
+    "url": "/monsters-rolodex/static/css/main.3539b0d5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae70f892148e0f733af8",
+    "url": "/monsters-rolodex/static/js/main.179de38f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
